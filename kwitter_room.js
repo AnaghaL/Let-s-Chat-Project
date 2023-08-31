a407ff7673e8544fb5317eabd576d5fb625f52d2
AnaghaL <95838394+AnaghaL@users.noreply.github.com>
2023-08-31 14:15:38 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABw3-ThwVEKNj_S0dcXrPbj1JoHTn2QDk",
+    authDomain: "let-s-chat-a89d7.firebaseapp.com",
+    projectId: "let-s-chat-a89d7",
+    storageBucket: "let-s-chat-a89d7.appspot.com",
+    messagingSenderId: "772101161931",
+    appId: "1:772101161931:web:57d8dea4d72bac6ad56c4e"
+  };
